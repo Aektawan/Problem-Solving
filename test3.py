94437648d0ada3f158d5cf161252bfd66f03b066
Aektawan <s6506026220133@email.kmutnb.ac.th>
2023-03-30 15:40:29 +0700

    d

--- test3.py
+++ test3.py
@@ -4,47 +4,32 @@ import matplotlib.pyplot as plt
 G = nx.Graph()
 
 G.add_edges_from([
-    ('RMUTT', 'NBU', {'distance': 9.5}),
-    ('RMUTT', 'EAU', {'distance': 4.5}),
-    ('RMUTT', 'VRU', {'distance': 35.9}),
-    ('RMUTT', 'TU', {'distance': 16.8}),
-    ('EAU', 'TU', {'distance': 20.3}),
-    ('EAU', 'BU', {'distance': 19.7}),
-    ('EAU', 'NBU', {'distance': 9.2}),
-    ('BU', 'TU', {'distance': 3.7}),
-    ('BU', 'NBU', {'distance': 17.5}),
-    ('BU', 'PTU', {'distance': 16.2}),
-    ('BU', 'RSU', {'distance': 18}),
-    ('TU', 'VRU', {'distance': 13.8}),
-    ('TU', 'PTU', {'distance': 15}),
-    ('TU', 'MRU', {'distance': 34.8}),
-    ('VRU', 'MRU', {'distance': 35.9}),
-    ('PTU', 'MRU', {'distance': 22}),
-    ('PTU', 'RPU', {'distance': 27.4}),
-    ('MRU', 'RPU', {'distance': 20.7}),
-    ('RPU', 'RSU', {'distance': 34.7}),
-    ('RSU', 'NBU', {'distance': 13.7}),
-    ('KMUTNB', 'RSU', {'distance': 30.3}),
-    ('KMUTNB', 'RPU', {'distance': 43.5}),
-    ('KMUTNB', 'MU', {'distance': 24.6}),
-    ('KMUTNB', 'CU', {'distance': 17.5}),
-    ('KMUTNB', 'NBU', {'distance': 18.1}),
-    ('MU', 'CU', {'distance': 29.2}),
-    ('MU', 'KMUTT', {'distance': 33.8}),
-    ('CU', 'KMUTT', {'distance': 15.1}),
-    ('CU', 'KMITL', {'distance': 33.3}),
-    ('KMITL', 'KMUTT', {'distance': 46.6}),
-    ('KMITL', 'NBU', {'distance': 39.2}),
+    ('RMUTT', 'NBU', {'distance': 9.5}),('RMUTT', 'EAU', {'distance': 4.5}),('RMUTT', 'VRU', {'distance': 35.9}),
+    ('RMUTT', 'TU', {'distance': 16.8}),('EAU', 'TU', {'distance': 20.3}),('EAU', 'BU', {'distance': 19.7}),
+    ('EAU', 'NBU', {'distance': 9.2}),('BU', 'TU', {'distance': 3.7}), ('BU', 'NBU', {'distance': 17.5}),('BU', 'PTU', {'distance': 16.2}),
+    ('BU', 'RSU', {'distance': 18}),('TU', 'VRU', {'distance': 13.8}),('TU', 'PTU', {'distance': 15}),('TU', 'MRU', {'distance': 34.8}),
+    ('VRU', 'MRU', {'distance': 35.9}),('PTU', 'MRU', {'distance': 22}),
+    ('PTU', 'RPU', {'distance': 27.4}),('MRU', 'RPU', {'distance': 20.7}),('RPU', 'RSU', {'distance': 34.7}),
+    ('RSU', 'NBU', {'distance': 13.7}),('KMUTNB', 'RSU', {'distance': 30.3}),
+    ('KMUTNB', 'RPU', {'distance': 43.5}),('KMUTNB', 'MU', {'distance': 24.6}),
+    ('KMUTNB', 'CU', {'distance': 17.5}),('KMUTNB', 'NBU', {'distance': 18.1}),('MU', 'CU', {'distance': 29.2}),
+    ('MU', 'KMUTT', {'distance': 33.8}),('CU', 'KMUTT', {'distance': 15.1}),('CU', 'KMITL', {'distance': 33.3}),
+    ('KMITL', 'KMUTT', {'distance': 46.6}),('KMITL', 'NBU', {'distance': 39.2}),
 ])
 
-pos = {'BU':(7,6.4),'RMUTT':(10.4,6),'TU':(7,8),'NBU':(8,3),'PTU':(5,6.5),'MRU':(2.5,8.8),'RSU':(5.3,4),'RPU':(3,6.5),'VRU':(9,9),'EAU':(8.6,6),'MU':(1,3),'KMUTNB':(3,3.5),'CU':(4.5,2),'KMUTT':(3.5,0.5),'KMITL':(8.5,1),}
-#G.add_nodes_from(['BU', 'RMUTT', 'TU', 'NBU', 'PTU', 'MRU', 'RSU', 'RPU', 'VRU', 'EAU', 'MU', 'KMUTNB', 'CU', 'KMUTT', 'KMITL'])
+pos = {'BU':(7,6.4),'RMUTT':(10.4,6),'TU':(7,8),'NBU':(9.5,4.3),'PTU':(5,6.5),'MRU':(2.5,8.8),'RSU':(6.5,4.5),
+       'RPU':(3,6.5),'VRU':(9,9),'EAU':(8.6,6),'MU':(1,3),'KMUTNB':(3,3.5),'CU':(4.5,2),'KMUTT':(3.5,0.5),'KMITL':(8.5,1),}
 
 def search_nodes():
     global shortest_path
-    print("['BU', 'RMUTT', 'TU', 'NBU', 'PTU', 'MRU', 'RSU', 'RPU', 'VRU', 'EAU', 'MU', 'KMUTNB', 'CU', 'KMUTT', 'KMITL']")
+    valid_nodes = ['BU', 'RMUTT', 'TU', 'NBU', 'PTU', 'MRU', 'RSU', 'RPU', 'VRU', 'EAU', 'MU', 'KMUTNB', 'CU', 'KMUTT', 'KMITL']
+    print(valid_nodes)
     source = input("Enter beginning from: ")
+    while source not in valid_nodes:
+        source = input(f"{source} is not a valid node. Please enter a valid source node: ")
     dest = input("Enter destination: ")
+    while dest not in valid_nodes:
+        dest = input(f"{dest} is not a valid node. Please enter a valid destination node: ")
     shortest_path = nx.shortest_path(G, source=source, target=dest, weight='distance')
     print(f"Shortest path from {source} to {dest}: {shortest_path}")
     shortest_path_length = nx.shortest_path_length(G, source=source, target=dest, weight='distance')
